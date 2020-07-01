6d0aadbfdea375f2a899efef873d87785a71cd3f
dima <dmitryure@gmail.com>
2020-07-01 13:03:36 +0300

    list done

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,14 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { ListItem } from "./components/ListItem";
 import style from "./List.module.css";
+import { v4 } from "uuid";
+import { useTransition } from "react-spring";
 
-export const List = (props) => {
-  const [items, setItems] = useState(["hello", "privet", "vasya"]);
+const modifyList = (id, set) => {
+  set(items => [...items.filter(el => el.id !== id)]);
+};
+
+export const List = props => {
+  const [items, setItems] = useState([
+    { value: "hello", id: v4() },
+    { value: "privet", id: v4() },
+    { value: "vasya", id: v4() }
+  ]);
+  const transitions = useTransition(items, item => item.id, {
+    from: { maxHeight: 0, transform: "translate3d(0px, 50px, 0px)", opacity: 0 },
+    enter: { maxHeight: 30, transform: "translate3d(0px, 0px, 0px)", opacity: 1 },
+    leave: { maxHeight: 0, transform: "translate3d(-50px, 0px, 0px)", opacity: 0 }
+  });
+
   return (
     <>
+      <button
+        onClick={() => setItems(items => [...items, { value: v4(), id: v4() }])}
+      >
+        add item
+      </button>
+      <button
+        onClick={event =>
+          setItems(items => [...items.filter(el => el === event.target.value)])
+        }
+      ></button>
       <div className={style.container}>
-        {items.map((item) => {
-          return <ListItem data={item} />;
+        {transitions.map(({ item, props, key }) => {
+          return (
+            <ListItem
+              setItems={setItems}
+              modifyList={modifyList}
+              key={key}
+              style={props}
+              data={item}
+            />
+          );
         })}
       </div>
       {props.children}
--- src/components/List/components/ListItem.jsx
+++ src/components/List/components/ListItem.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { animated } from "react-spring";
+import classNames from "./ListItem.module.css";
 
-export const ListItem = (props) => {
-  return <div>{props.data}</div>;
+export const ListItem = ({ data, style, setItems, modifyList, key }) => {
+  console.log(data);
+  return (
+    <animated.div
+      className={classNames.container}
+      key={key}
+      onClick={() => modifyList(data.id, setItems)}
+      style={style}
+    >
+      {data.value}
+    </animated.div>
+  );
 };
